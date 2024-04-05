2a2c93cdcfb35642cc37408a77b6f672bceea8b1
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-04-05 22:23:32 +1000

    ♻️ Added data model for daily scrum

--- src/WebUI/Components/Components/Bar.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace WebUI.Components.Components;
-
-public partial class Bar : ComponentBase
-{
-}
---
+++ src/WebUI/Features/DailyScrum/DailyScrumModel.cs
@@ -0,0 +1,32 @@
+﻿namespace WebUI.Features.DailyScrum;
+
+public class DailyScrumModel
+{
+    public int DaysUntilNextBooking { get; init; }
+    public int InboxCount { get; init; }
+    public required string TrelloBoardUrl { get; init; }
+    public List<ProjectModel> YesterdaysProjects { get; init; } = new();
+    public List<TaskModel> YesterdaysTasks { get; init; } = new();
+    public List<ProjectModel> TodaysProjects { get; init; } = new();
+    public List<TaskModel> TodaysTasks { get; init; } = new();
+}
+
+public class TaskModel
+{
+    public TaskStatus Status { get; init; }
+    public required string Name { get; init; }
+}
+
+public class ProjectModel
+{
+    public string Name { get; init; } = null!;
+    public List<TaskModel> Tasks { get; init; } = new();
+}
+
+public enum TaskStatus
+{
+    Todo = 1,
+    InProgress = 2,
+    Done = 3,
+    Blocked = 4
+}
