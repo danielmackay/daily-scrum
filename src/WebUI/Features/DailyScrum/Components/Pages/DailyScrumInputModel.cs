2d9665e74f27f6752a972a40086ea8ec72972edb
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-04-14 13:37:30 +1000

    🐛 Fixed bug with InputText component and nullable int.

--- src/WebUI/Features/DailyScrum/Components/Pages/DailyScrumInputModel.cs
+++ src/WebUI/Features/DailyScrum/Components/Pages/DailyScrumInputModel.cs
@@ -1,10 +1,15 @@
-﻿namespace WebUI.Features.DailyScrum.Components.Pages;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Features.DailyScrum.Components.Pages;
 
 public class DailyScrumInputModel
 {
+    [Required]
     public string Name { get; set; } = String.Empty;
 
-    public int? ClientDays { get; set; }
+    // NOTE: THis needs to be a string due to the following bug with nullable fields
+    // https://github.com/dotnet/aspnetcore/issues/52195
+    public string ClientDays { get; set; } = String.Empty;
 
     public DateOnly LastWorkingDay { get; set; } = DateOnly.FromDateTime(DateTime.Now.AddDays(-1));
 }
