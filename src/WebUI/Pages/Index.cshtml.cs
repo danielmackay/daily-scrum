f778f936b234172d7ef40679a134f641cb4d8d27
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-12-10 20:25:04 +1000

    Remove unneeded code from IndexPage

--- src/WebUI/Pages/Index.cshtml.cs
+++ src/WebUI/Pages/Index.cshtml.cs
@@ -5,21 +5,6 @@ using WebUI.Features.DailyScrum.UseCases.CreateDailyScrumCommand;
 
 namespace WebUI.Pages;
 
-// [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
 public class IndexModel : PageModel
 {
-    // private readonly GraphServiceClient _graphServiceClient;
-
-    public IndexModel(
-        GraphServiceClientFactory factory)
-    {
-        // _graphServiceClient = factory.CreateWithAccessToken();
-    }
-
-    public async Task OnGet()
-    {
-        // var user = await _graphServiceClient.Me.GetAsync();
-        //
-        // ViewData["ApiResult"] = user?.DisplayName;
-    }
 }
