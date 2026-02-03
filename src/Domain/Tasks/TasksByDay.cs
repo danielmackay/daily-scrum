d91e7a23cdb421f45a4d937d2c22bf86289e408b
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2026-02-04 09:37:13 +1000

    ♻️ Add DayOfWeek property to TasksByDay and clarify date handling

--- src/Domain/Tasks/TasksByDay.cs
+++ src/Domain/Tasks/TasksByDay.cs
@@ -3,7 +3,11 @@ namespace Domain;
 // TODO: This could be a DTO
 public class TasksByDay
 {
+    // DM: This returns the correct date, but the day is always one off.
     public DateOnly Date { get; }
+
+    public string DayOfWeek => Date.DayOfWeek.ToString();
+
     public List<Project> Projects { get; }
 
     public TasksByDay(DateOnly date, List<Project> projects)
